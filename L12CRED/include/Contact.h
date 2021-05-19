a4ba9035dda584ae94be209a8580ac122d76dfcb
Liviu Andrei <petrache.andrei1@gmail.com>
2021-05-19 18:39:13 +0300

    Added the skeleton

---
+++ L12CRED/include/Contact.h
@@ -0,0 +1,8 @@
+//
+// Created by andrew on 19.05.2021.
+//
+
+#ifndef L12CRED_CONTACT_H
+#define L12CRED_CONTACT_H
+
+#endif //L12CRED_CONTACT_H
---
+++ L12CRED/include/Prieten.h
@@ -0,0 +1,8 @@
+//
+// Created by andrew on 19.05.2021.
+//
+
+#ifndef L12CRED_PRIETEN_H
+#define L12CRED_PRIETEN_H
+
+#endif //L12CRED_PRIETEN_H
